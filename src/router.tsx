da9f5b6a463aa14b091282f5ecc4a1ab6f93dd60
jk <jkwak1635@gmail.com>
2024-12-24 02:57:19 +0900

    feat: 주요 페이지(home, search, chats) 라우팅 설정

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Home from './pages/home/index';
+import Search from './pages/search/index';
+import Chats from './pages/chats/index';
+
 type routeElement = {
   path: string;
   element: React.ReactNode;
@@ -6,6 +10,10 @@ type routeElement = {
   children?: { path: string; element: React.ReactNode }[];
 };
 
-const routes: routeElement[] = [];
+const routes: routeElement[] = [
+  { path: '/', element: <Home /> },
+  { path: '/search', element: <Search /> },
+  { path: '/chats', element: <Chats /> },
+];
 
 export const router = createBrowserRouter(routes);
