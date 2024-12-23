19718a7b11459df2f7df939ecf0bd5ae5d063bf0
jk <jkwak1635@gmail.com>
2024-12-23 13:17:29 +0900

    chore: 경로 오류 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import GlobalStyle from './styles/GlobalStyle.ts';
+import GlobalStyle from './common/styles/GlobalStyle.ts';
 import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme.ts';
+import theme from './common/styles/theme.ts';
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
