1ccc48f89c84bb4039542d9779b2ef371f3852d4
jk <jkwak1635@gmail.com>
2024-12-24 23:40:21 +0900

    feat: colors, fontWeight, typography 설정 추가

--- src/common/styles/theme.ts
+++ src/common/styles/theme.ts
@@ -1,21 +1,36 @@
 const colors = {
+  white: '#FFFFFF',
   black: '#211708',
-  brown: '#b57d2e',
-  orange: '#ff9900',
-  yellow: '#fee500',
+  gray: '#E5E8EB',
+  orange100: '#FAF5F0',
+  orange200: '#F5EBD9',
+  orange300: '#F0DEC2',
+  orange400: '#FF9900',
+  orange500: '#B57D2E',
 };
 
-const fontSizes = {
-  xs: '0.875rem',
-  small: '1rem',
-  base: '1.25rem',
-  large: '1.375rem',
-  xl: '1.75rem',
+const fontWeight = {
+  regular: 400,
+  medium: 500,
+  semiBold: 600,
+  bold: 700,
+};
+
+const typography = {
+  t12: { fontSize: '0.75rem', lineHeight: 1.5 },
+  t13: { fontSize: '0.813rem', lineHeight: 1.6 },
+  t14: { fontSize: '0.875rem', lineHeight: 1.6 },
+  t15: { fontSize: '0.938rem', lineHeight: 1.5 },
+  t16: { fontSize: '1rem', lineHeight: 1.5 },
+  t18: { fontSize: '1.125rem', lineHeight: 1.5 },
+  t22: { fontSize: '1.375rem', lineHeight: 1.5 },
+  t28: { fontSize: '1.75rem', lineHeight: 1.4 },
 };
 
 const theme = {
   colors,
-  fontSizes,
+  fontWeight,
+  typography,
 };
 
 export default theme;
