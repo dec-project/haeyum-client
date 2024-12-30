97f33e8cbbbe23569bb1839692845c64f8a1f90a
ohamin26 <ohamin26@gmail.com>
2024-12-30 23:05:01 +0900

    fix : Calendar - PickerType 타입 변경

--- src/common/components/calendar/index.tsx
+++ src/common/components/calendar/index.tsx
@@ -3,18 +3,16 @@ import { Dispatch, SetStateAction } from 'react';
 import CalendarLayout from './CalendarLayout';
 import DateDisplay from './DateDisplay';
 
-export type PickerType = 'date' | 'month' | 'year' | '';
+export type PickerType = 'date' | 'month' | 'year';
 export interface DatePickerProps {
   setPickerType: Dispatch<SetStateAction<PickerType>>;
 }
 
 export default function Calendar() {
-  // const { startDate, endDate, setStartDate, setEndDate } = useCalendarStore();
   return (
     <div>
       <DateDisplay />
       <CalendarLayout />
-      {/* <CalendarLayout selectedDate={endDate || new Date()} setSelectedDate={setEndDate} setPickerType={() => {}} /> */}
     </div>
   );
 }
