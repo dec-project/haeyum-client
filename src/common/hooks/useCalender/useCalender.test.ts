d6a18c34e352f37abb96ed953ce10a7292ac208b
ohamin26 <ohamin26@gmail.com>
2025-01-01 19:10:26 +0900

    fix : useCalender 훅 수정에 따른 테스트 코드 수정

--- src/common/hooks/useCalender/useCalender.test.ts
+++ src/common/hooks/useCalender/useCalender.test.ts
@@ -1,37 +1,50 @@
 import { renderHook } from '@testing-library/react-hooks';
 import useCalender from './useCalender';
-import { format, addDays, startOfWeek } from 'date-fns';
+import { format, addDays, startOfWeek, startOfMonth, endOfMonth, eachDayOfInterval, endOfWeek } from 'date-fns';
 
 describe('useCalender Hook', () => {
   it('요일 포맷 테스트', () => {
-    const { result } = renderHook(() => useCalender(new Date()));
+    const { result } = renderHook(() => useCalender(new Date(), new Date()));
 
     const weekStartDate = startOfWeek(new Date());
     const expectedWeekDays = [];
     for (let day = 0; day < 7; day += 1) {
       expectedWeekDays.push(format(addDays(weekStartDate, day), 'EEEEE'));
     }
-    console.log(result.current.weekDays);
+
     expect(result.current.weekDays).toEqual(expectedWeekDays);
   });
 
   it('12개월이 정상적으로 반환되는지 테스트', () => {
     const selectedDate = new Date('2024-01-01');
-    const { result } = renderHook(() => useCalender(selectedDate));
+    const { result } = renderHook(() => useCalender(selectedDate, selectedDate));
 
-    console.log(result.current.allMonth);
     expect(result.current.allMonth.length).toBe(12);
     expect(result.current.allMonth[0].getMonth()).toBe(0);
     expect(result.current.allMonth[11].getMonth()).toBe(11);
   });
 
-  it('날짜가 전부 정상적으로 반환되는지 테스트', () => {
-    const selectedDate = new Date('2024-01-15');
-    const { result } = renderHook(() => useCalender(selectedDate));
+  it('시작 월의 날짜가 정상적으로 반환되는지 테스트', () => {
+    const startDate = new Date('2024-01-01');
+    const { result } = renderHook(() => useCalender(startDate, new Date()));
 
-    console.log(result.current.currentMonthAllDates);
-    const { currentMonthAllDates } = result.current;
-    expect(currentMonthAllDates[0].getDay()).toBe(0);
-    expect(currentMonthAllDates[currentMonthAllDates.length - 1].getDay()).toBe(6);
+    const expectedDates = eachDayOfInterval({
+      start: startOfWeek(startOfMonth(startDate)),
+      end: endOfWeek(endOfMonth(startDate)),
+    });
+
+    expect(result.current.startMonthDates).toEqual(expectedDates);
+  });
+
+  it('끝 월의 날짜가 정상적으로 반환되는지 테스트', () => {
+    const endDate = new Date('2024-02-01');
+    const { result } = renderHook(() => useCalender(new Date(), endDate));
+
+    const expectedDates = eachDayOfInterval({
+      start: startOfWeek(startOfMonth(endDate)),
+      end: endOfWeek(endOfMonth(endDate)),
+    });
+
+    expect(result.current.endMonthDates).toEqual(expectedDates);
   });
 });
