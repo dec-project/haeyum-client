9a760b711e253839761ce46d503f3c0c8c0a58f3
ohamin26 <ohamin26@gmail.com>
2025-01-02 22:53:59 +0900

    fix : eslint 설정 변경

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
 import prettier from 'eslint-plugin-prettier/recommended';
 export default tseslint.config(
   {
-    ignores: ['**/build/**', '**/dist/**', '**/.yarn/**', '**/public/**', '**/.pnp.cjs', '**/.pnp.loader.mjs', "eslint.config.js"],
+    ignores: ['**/build/**', '**/dist/**', '**/.yarn/**', '**/public/**', '**/.pnp.cjs', '**/.pnp.loader.mjs'],
   },
   eslint.configs.recommended,
   prettier,
