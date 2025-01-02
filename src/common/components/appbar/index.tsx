55b0d3fc086d5663d51d7e691161d3bd034288db
ohamin26 <ohamin26@gmail.com>
2025-01-02 22:57:31 +0900

    feat : Appbar layout 추가

--- src/common/components/appbar/index.tsx
+++ src/common/components/appbar/index.tsx
@@ -1,5 +1,57 @@
-const AppBar = () => {
-  return <div>index</div>;
+import styled from 'styled-components';
+
+interface AppBarProps {
+  leftContent?: React.ReactNode;
+  centerContent?: React.ReactNode;
+  rightContent?: React.ReactNode;
+}
+
+const AppBar = ({ leftContent, centerContent, rightContent }: AppBarProps) => {
+  return (
+    <Container>
+      <Left>
+        {leftContent} {centerContent}
+      </Left>
+      <Right>
+        <Icon>{rightContent}</Icon>
+      </Right>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: auto;
+  right: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: none;
+  padding: 16px;
+  width: 100%;
+  height: 72px;
+  max-width: var(--max-width);
+  min-width: var(--min-width);
+  span {
+    ${({ theme }) => theme.typography.title3.bold}
+  }
+`;
+
+const Left = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const Right = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Icon = styled.div`
+  size: 24px;
+  cursor: pointer;
+`;
 
 export default AppBar;
