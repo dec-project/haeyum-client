c0f164f194988658e300e882a8fa7ccdfeb66424
jk <jkwak1635@gmail.com>
2025-01-03 11:31:30 +0900

    feat: api, interceptors 추가

---
+++ src/common/apis/api.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosRequestConfig, AxiosInstance } from 'axios'
+import {
+  errorInterceptor,
+  requestInterceptor,
+  successInterceptor,
+} from './interceptors';
+
+export interface ConsoleError {
+  status: number;
+  data: unknown; 
+} 
+  
+const axiosRequestConfig: AxiosRequestConfig = {
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+const api: AxiosInstance = axios.create(axiosRequestConfig);
+  
+api.interceptors.request.use(requestInterceptor);
+api.interceptors.response.use(successInterceptor, errorInterceptor);
+
+export default api;
