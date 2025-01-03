d636b751d469c10b578c24b010efad3b1547f0ce
ohamin26 <ohamin26@gmail.com>
2025-01-03 19:38:22 +0900

    feat : AppBar 필요한 버튼 추가

---
+++ src/common/components/appbar/CommonStyle.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  img {
+    width: 19px;
+    height: 19px;
+  }
+`;
+
+export { Button };
