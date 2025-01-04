cae3b6285857fceeccd43a3b3456635c3e54e6d9
jk <jkwak1635@gmail.com>
2025-01-04 23:36:43 +0900

    feat: mock data 추가

---
+++ src/pages/trip/index/components/data.ts
@@ -0,0 +1,86 @@
+const weatherData = {
+  weather: '흐림',
+  img: 'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+};
+
+const movieData = {
+  itemList: [
+    {
+      movieId: 1,
+      ranking: 1,
+      title: '조커',
+      img: 'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      movieId: 2,
+      ranking: 2,
+      title: '모아나',
+      img: 'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      movieId: 3,
+      ranking: 3,
+      title: '인셉션',
+      img: 'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      movieId: 4,
+      ranking: 4,
+      title: '인터스텔라',
+      img: 'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      movieId: 5,
+      ranking: 5,
+      title: '다크 나이트',
+      img: 'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+  ],
+};
+
+const musicData = {
+  songSummaries: [
+    {
+      songId: 1,
+      title: 'Rolling in the Deep',
+      artist: ['Adele'],
+      ranking: 1,
+      imgUrl:
+        'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      songId: 2,
+      title: 'Bohemian Rhapsody',
+      artist: ['Queen'],
+      ranking: 2,
+      imgUrl:
+        'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      songId: 3,
+      title: 'Hotel California',
+      artist: ['Eagles'],
+      ranking: 3,
+      imgUrl:
+        'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      songId: 4,
+      title: 'Blinding Lights',
+      artist: ['The Weeknd'],
+      ranking: 4,
+      imgUrl:
+        'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      songId: 5,
+      title: "I Don't Think That I Like Her",
+      artist: ['Charlie Puth'],
+      ranking: 5,
+      imgUrl:
+        'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+  ],
+};
+
+export { weatherData, movieData, musicData };
