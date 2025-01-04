b53c7cd6d2d932a272f85564ed10838c18eaf9ad
jk <jkwak1635@gmail.com>
2025-01-04 23:50:05 +0900

    chore: movie 이미지 사이즈 수정

--- src/pages/trip/index/components/data.ts
+++ src/pages/trip/index/components/data.ts
@@ -3,82 +3,82 @@ const weatherData = {
   img: 'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 };
 
+const musicData = {
+  songSummaries: [
+    {
+      songId: 1,
+      title: 'Die With A Smile',
+      artist: ['Lady Gaga', 'Bruno Mars'],
+      ranking: 1,
+      imgUrl:
+        'https://cdnimg.melon.co.kr/cm2/album/images/115/68/901/11568901_20240816130602_500.jpg/melon/resize/120/quality/80/optimize',
+    },
+    {
+      songId: 2,
+      title: 'STAY',
+      artist: ['The Kid LAROI', 'Justin Bieber'],
+      ranking: 2,
+      imgUrl:
+        'https://cdnimg.melon.co.kr/cm2/album/images/106/46/395/10646395_20210707141710_500.jpg/melon/resize/120/quality/80/optimize',
+    },
+    {
+      songId: 3,
+      title: 'Dangerously',
+      artist: ['Charlie Puth'],
+      ranking: 3,
+      imgUrl:
+        'https://cdnimg.melon.co.kr/cm/album/images/023/36/926/2336926_500.jpg/melon/resize/120/quality/80/optimize',
+    },
+    {
+      songId: 4,
+      title: 'Older',
+      artist: ['Sasha Alex Sloan'],
+      ranking: 4,
+      imgUrl:
+        'https://cdnimg.melon.co.kr/cm/album/images/102/20/418/10220418_500.jpg/melon/resize/120/quality/80/optimize',
+    },
+    {
+      songId: 5,
+      title: 'Beautiful Things',
+      artist: ['Benson Boone'],
+      ranking: 5,
+      imgUrl:
+        'https://cdnimg.melon.co.kr/cm2/album/images/114/01/422/11401422_20240930152842_500.jpg/melon/resize/120/quality/80/optimize',
+    },
+  ],
+};
+
 const movieData = {
   itemList: [
     {
       movieId: 1,
       ranking: 1,
       title: '조커',
-      img: 'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      img: 'https://upload.wikimedia.org/wikipedia/ko/3/31/%EC%A1%B0%EC%BB%A4_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg',
     },
     {
       movieId: 2,
       ranking: 2,
       title: '모아나',
-      img: 'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      img: 'https://www.chosun.com/resizer/v2/I5NQAWGL7RDMZBUKX52QLL4RWA.jpg?auth=dff6f26b2c40bedff43f2244d0bda5cd4b2a4c08a53190846e9fbe97fd8bd9e8&width=1280',
     },
     {
       movieId: 3,
       ranking: 3,
       title: '인셉션',
-      img: 'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      img: 'https://upload.wikimedia.org/wikipedia/ko/1/1d/%EC%9D%B8%EC%85%89%EC%85%98.jpg',
     },
     {
       movieId: 4,
       ranking: 4,
       title: '인터스텔라',
-      img: 'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      img: 'https://i.namu.wiki/i/qnfv6dUwAyq5eu94FAsUB5FQZ4QY7y6nZk2f5NtoahVUx09rr9ikkghsa3ZaqGD0ZhIfWi6kKi9HBMgZ7Nfc8w.webp',
     },
     {
       movieId: 5,
       ranking: 5,
       title: '다크 나이트',
-      img: 'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-  ],
-};
-
-const musicData = {
-  songSummaries: [
-    {
-      songId: 1,
-      title: 'Rolling in the Deep',
-      artist: ['Adele'],
-      ranking: 1,
-      imgUrl:
-        'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      songId: 2,
-      title: 'Bohemian Rhapsody',
-      artist: ['Queen'],
-      ranking: 2,
-      imgUrl:
-        'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      songId: 3,
-      title: 'Hotel California',
-      artist: ['Eagles'],
-      ranking: 3,
-      imgUrl:
-        'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      songId: 4,
-      title: 'Blinding Lights',
-      artist: ['The Weeknd'],
-      ranking: 4,
-      imgUrl:
-        'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      songId: 5,
-      title: "I Don't Think That I Like Her",
-      artist: ['Charlie Puth'],
-      ranking: 5,
-      imgUrl:
-        'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      img: 'https://i.namu.wiki/i/K0jk_ckp7uB8kISisIwUlW8G56hgQ6V-mtmOCCpNjgZ5JlSAq0GGDS9YJf7nlvXgEW2vwU6gvCAZeWpVw_-jFA.webp',
     },
   ],
 };
