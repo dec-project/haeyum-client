134229517be364004c444ad471df13aea3980218
jk <jkwak1635@gmail.com>
2025-01-04 17:01:39 +0900

    feat: trip 페이지 하단 버튼 추가

---
+++ src/pages/trip/index/index.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import FixedBottom from '@/common/components/fixedBottom';
+import IconChatDot from '@/common/assets/icon/icon-chat-dot.svg';
+
+const Trip = () => {
+  return (
+    <div>
+      <h1>Trips</h1>
+      <FixedBottom>
+        <ButtonWrapper>
+          <Button>
+            <Icon src={IconChatDot} />
+            <span>90년대 채팅방</span>
+          </Button>
+        </ButtonWrapper>
+      </FixedBottom>
+    </div>
+  );
+};
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Button = styled.button`
+  display: flex;
+  gap: 16px;
+  padding: 16px;
+  background-color: ${({ theme }) => theme.themeColors.secondary};
+  border-radius: 4px;
+  ${({ theme }) => theme.typography.body1.bold};
+
+  & > span {
+    margin-top: 1px;
+  }
+`;
+
+const Icon = styled.img``;
+
+export default Trip;
