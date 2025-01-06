e77997d68eaa516dcc25fb718c2c39e6183e579b
ohamin26 <ohamin26@gmail.com>
2025-01-06 21:47:33 +0900

    fix : icon 사이즈 및 스타일 수정

--- src/common/components/appbar/CommonStyle.tsx
+++ src/common/components/appbar/CommonStyle.tsx
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 const Button = styled.button`
   background-color: transparent;
   border: none;
+  margin: 0;
+  padding: 0;
   cursor: pointer;
   img {
-    width: 19px;
-    height: 19px;
+    width: 100%;
+    height: 100%;
   }
 `;
 
