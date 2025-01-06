4b27cd4e5893bb01d2c5f7bda27e678bdfd7d597
jk <jkwak1635@gmail.com>
2025-01-07 03:30:07 +0900

    feat: trip 페이지 추가

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+
 import HomePage from './pages/home/index';
 import SearchPage from './pages/search/index';
 import ChatsPage from './pages/chats/index';
+import TripPage from './pages/trip/index';
 
 type routeElement = {
   path: string;
@@ -15,6 +17,7 @@ const routes: routeElement[] = [
   { path: '/', element: <HomePage /> },
   { path: '/chats', element: <ChatsPage /> },
   { path: '/search', element: <SearchPage /> },
+  { path: '/trip', element: <TripPage /> },
 ];
 
 export const router = createBrowserRouter(routes);
