6a2aac39f26c570b1357fdfcec5563dc97688e41
ohamin26 <ohamin26@gmail.com>
2025-01-07 20:39:08 +0900

    feat : 홈 페이지 레이아웃 작성

--- src/pages/home/index/index.tsx
+++ src/pages/home/index/index.tsx
@@ -1,5 +1,49 @@
+import styled from 'styled-components';
+import Carousel from './components/carousel';
+import Layout from './components/layout';
+import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
-  return <div>home</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Layout>
+      <Container>
+        <SearchInput placeholder="어느 시기로 가볼까요" onClick={() => navigate('/search')} />
+        <Text>
+          <span>실시간 인기 채팅방</span>
+        </Text>
+        <Carousel />
+        <Text>
+          <span>실시간 인기 검색 Top 5</span>
+        </Text>
+      </Container>
+    </Layout>
+  );
 };
+
+const Container = styled.div`
+  padding: 1rem;
+`;
+
+const SearchInput = styled.input`
+  margin-bottom: 12px;
+  padding: 0 15px;
+  width: 100%;
+  height: 48px;
+  border: 1px solid ${({ theme }) => theme.themeColors.border};
+  border-radius: 4px;
+  ${({ theme }) => theme.typography.body1.regular};
+  &::placeholder {
+    color: ${({ theme }) => theme.themeColors.textSecondary};
+  }
+`;
+
+const Text = styled.div`
+  padding: 20px 0 12px 0;
+  span {
+    ${({ theme }) => theme.typography.title2.bold};
+  }
+`;
 
 export default Home;
