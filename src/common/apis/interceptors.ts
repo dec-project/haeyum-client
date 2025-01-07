8b32b983ba024fbe809c44a200df17d00c885de8
jk <jkwak1635@gmail.com>
2025-01-07 13:45:26 +0900

    fix: errorInterceptor 수정

--- src/common/apis/interceptors.ts
+++ src/common/apis/interceptors.ts
@@ -13,47 +13,53 @@ export const successInterceptor = (response: AxiosResponse): AxiosResponse => {
   return response.data;
 };
 
-export class CustomError extends Error {
+export class ApiError extends Error {
+  public statusCode: number;
   public details?: any;
 
-  constructor(message: string, details?: any) {
-    super(message);
-    this.details = details;
-    this.name = this.constructor.name;
-  }
-}
-
-export class ApiError extends CustomError {
-  public statusCode: number;
-
   constructor(statusCode: number, message: string, details?: any) {
-    super(message, details);
+    super(message);
     this.statusCode = statusCode;
+    this.details = details;
     this.name = 'ApiError';
   }
 }
 
-export class BusinessLogicError extends CustomError {
-  constructor(message: string, details?: any) {
-    super(message, details);
-    this.name = 'BusinessLogicError';
+export class ClientError extends ApiError {
+  constructor(statusCode: number, message: string, details?: any) {
+    super(statusCode, message, details);
+    this.name = 'ClientError';
   }
 }
 
-export const errorInterceptor = async (error: AxiosError): Promise<void> => {
+export class ServerError extends ApiError {
+  constructor(statusCode: number, message: string, details?: any) {
+    super(statusCode, message, details);
+    this.name = 'ServerError';
+  }
+}
+
+interface ErrorResponse {
+  code: string;
+  message: string;
+}
+
+export const errorInterceptor = async (error: AxiosError<ErrorResponse>): Promise<void> => {
   if (error.response) {
-    const apiError = new ApiError(error.response.status, '서버에서 오류가 발생했습니다.', error.response.data);
+    const { status, data } = error.response;
+
+    const apiError =
+      status >= 400 && status < 500
+        ? new ClientError(status, data?.message || '클라이언트 오류가 발생했습니다.', data)
+        : new ServerError(status, data?.message || '서버 처리 중 오류가 발생했습니다.', data);
+
     console.error(apiError);
-    await Promise.reject(apiError);
+    return Promise.reject(apiError);
   } else if (error.request) {
-    const apiError = new ApiError(500, '서버로부터 응답을 받지 못했습니다.', error.request);
-    console.error(apiError);
-    await Promise.reject(apiError);
+    const apiError = new ApiError(0, '요청은 완료했으나, 서버로부터 응답을 받지 못했습니다.', error.request);
+    return Promise.reject(apiError);
   } else {
-    const businessLogicError = new BusinessLogicError('요청 설정 중 문제가 발생했습니다.', {
-      originalMessage: error.message,
-    });
-    console.error(businessLogicError);
-    await Promise.reject(businessLogicError);
+    const apiError = new ApiError(0, '서버로부터 응답이 없습니다.', error.message);
+    return Promise.reject(apiError);
   }
 };
