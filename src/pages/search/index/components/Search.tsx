a43122e1cc9942e30d68f729f54034101c49b358
ohamin26 <ohamin26@gmail.com>
2025-01-08 20:50:34 +0900

    feat : 검색 페이지 스타일링

--- src/pages/search/index/components/Search.tsx
+++ src/pages/search/index/components/Search.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Calendar from '../../../../common/components/calendar';
+import Calendar from '@/common/components/calendar';
 import { format } from 'date-fns';
+import styled from 'styled-components';
+import AppBar from '@/common/components/appbar';
 
 const Search = () => {
   const navigate = useNavigate();
@@ -21,13 +23,52 @@ const Search = () => {
   };
 
   return (
-    <div>
-      <Calendar startDate={startDate} endDate={endDate} setEndDate={setEndDate} setStartDate={setStartDate} />
-      <button onClick={handleSearch} disabled={!startDate || !endDate}>
-        GO
-      </button>
-    </div>
+    <Wrapper>
+      <AppBar text="검색" />
+      <Container>
+        <Calendar startDate={startDate} endDate={endDate} setEndDate={setEndDate} setStartDate={setStartDate} />
+        <ButtonBox>
+          <Button onClick={handleSearch} disabled={!startDate || !endDate}>
+            <span>GO</span>
+          </Button>
+        </ButtonBox>
+      </Container>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  width: fit-content;
+  height: fit-content;
+  margin: 0 auto;
+`;
+
+const Container = styled.div`
+  padding-top: 72px;
+  width: fit-content;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  & > button {
+    margin: 0 16px;
+  }
+`;
+
+const ButtonBox = styled.div`
+  padding: 12px 16px;
+`;
+
+const Button = styled.button`
+  width: 84px;
+  height: 40px;
+  border-radius: 7px;
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+  background-color: ${({ theme, disabled }) => (disabled ? theme.colors.gray : theme.themeColors.secondary)};
+  span {
+    color: ${({ theme, disabled }) => (disabled ? theme.colors.white : theme.colors.white)};
+    ${({ theme }) => theme.typography.body1.bold};
+  }
+`;
 
 export default Search;
