dffdc5faa4d0f394985cc93957e1a3bac44b4842
jk <jkwak1635@gmail.com>
2025-01-10 13:46:53 +0900

    feat: 노래/영화 상세 타입 추가

---
+++ src/pages/trip/detail/index/types/index.ts
@@ -0,0 +1,22 @@
+export interface MovieInfo {
+  title: string;
+  content: string;
+  releaseDate: string;
+  youtubeAddr: string;
+}
+
+export interface MusicInfo {
+  songId: number;
+  title: string;
+  artists: string;
+  ranking: number;
+  releaseDate: string;
+  genre: string;
+  albumTitle: string;
+  lyrics: string;
+  lyricists: string;
+  composers: string;
+  arrangers: string;
+  youtubeAddr: string;
+  description: string;
+}
