455b78fbdaab773987c78a105d66d847dce380f2
jk <jkwak1635@gmail.com>
2025-01-10 13:44:48 +0900

    feat: 상세 dummy 데이터 추가

---
+++ src/pages/trip/detail/index/components/data.ts
@@ -0,0 +1,24 @@
+const movieInfoData = {
+  title: '소방관',
+  content:
+    '소방관은 2024년 12월 4일 개봉한 대한민국의 영화이다. 2001년 홍제동 주택 화재 사고에 실화를 바탕으로 한 작품이다.',
+  releaseDate: '2024-12-04',
+  youtubeAddr: 'UmagF8Tmk6E',
+};
+
+const musicInfoData = {
+  songId: 38242510,
+  title: 'APT.',
+  artists: 'artist A, artist B',
+  ranking: 1,
+  releaseDate: '2024년 10월 18일',
+  genre: '댄스/팝, 락/메탈',
+  albumTitle: '앨범 이름',
+  lyrics: `You know what?<br>Not even God can stop me<br>I'll like you<br><br>I'm a diamond<br>너무 소중한 내 마음은<br>너를 볼 때 빛나 I can't<br>I can't stop myself<br><br>Am I selfish?<br>눈치 없는 니 맘 대신<br>지금 이 설렘이 훨씬<br>더 내겐 소중해<br><br>Boy, I ch-ch-ch-ch-cherish my love (my love)<br>Ch-ch-ch-ch-cherish my love<br>Oh, my crush, that's all my wish<br>빛이 나는 이 설렘은 못 참지<br>협조해줘 you gotta<br><br>Ch-ch-ch-ch-cherish my love<br>어차피 넌 내 게, 내 게 분명<br>날 말릴 순 없을걸<br>하느님도 두 손 두 발 들었지<br>좋아해 'cause I wanna<br><br>너란 아이는 make me higher<br>널 웃기면<br>웃을까? 어떨까? 이상하게 볼까?<br>상상만으로 난 oh, cherish my love<br><br>If you, if you, if you love me or not<br>궁금해 너의 맘<br>고민은 3초만 난 my way<br>니 맘은 두 번째<br>내 각오가 첫 번째<br>절대 끝까지 지켜, 내 감정<br><br>Boy, I ch-ch-ch-ch-cherish my love (my love)<br>Ch-ch-ch-ch-cherish my love<br>Oh, my crush, that's all my wish<br>빛이 나는 이 설렘은 못 참지<br>협조해줘 you gotta<br><br>Ch-ch-ch-ch-cherish my love<br>어차피 넌 내 게, 내 게 분명<br>날 말릴 순 없을걸<br>하느님도 두 손 두 발 들었지<br>좋아해 'cause I wanna<br><br>Cherish 이 설렘, 넌 cherry on top<br>Cherish 내 욕심이 원해 you're mine<br>알쏭달쏭 너의 표정에 괜히 불타올라<br>무조건 직진할래<br><br>Boy, I ch-ch-ch-ch-cherish my love (my love)<br>Ch-ch-ch-ch-cherish my love<br>Oh, my crush, that's all my wish<br>빛이 나는 이 설렘은 못 참지<br>협조해줘 you gotta<br><br>Ch-ch-ch-ch-cherish my love<br>어차피 넌 내 게, 내 게 분명<br>날 말릴 순 없을걸<br>하느님도 두 손 두 발 들었지<br>좋아해 'cause I wanna<br><br>I'm a diamond<br>너무 소중한 내 마음은<br>너를 향해 빛나<br>네게 건네줄 거야<br></br>`,
+  lyricists: 'lyricists A, lyricists B',
+  composers: 'composers A, composers B',
+  arrangers: 'arrangers A, arrangers B',
+  youtubeAddr: 'ekr2nIex040',
+};
+
+export { movieInfoData, musicInfoData };
