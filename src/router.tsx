ea508d6b0bcb91cf2f24ffb28f29a9c3e9c44643
ohamin26 <ohamin26@gmail.com>
2025-01-11 15:05:37 +0900

    feat : 마이페이지 라우팅 추가

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,8 @@ import HomePage from './pages/home/index';
 import SearchPage from './pages/search/index';
 import ChatsPage from './pages/chats/index';
 import TripPage from './pages/trip/index';
+import Navbar from './common/components/navbar';
+import MyPage from './pages/my/index';
 
 type routeElement = {
   path: string;
@@ -13,9 +15,16 @@ type routeElement = {
 };
 
 const routes: routeElement[] = [
-  { path: '/', element: <HomePage /> },
-  { path: '/search', element: <SearchPage /> },
-  { path: '/chats', element: <ChatsPage /> },
+  {
+    path: '/',
+    element: <Navbar />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/search', element: <SearchPage /> },
+      { path: '/chats', element: <ChatsPage /> },
+      { path: '/profile', element: <MyPage /> },
+    ],
+  },
   { path: '/trip/:calendarId', element: <TripPage /> },
 ];
 
