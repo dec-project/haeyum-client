3814512a0b797a6bf82c21632e9f812f4e261a7b
jk <jkwak1635@gmail.com>
2025-01-12 07:12:35 +0900

    fix: rankingApi 타입 에러 수정

--- src/common/apis/ranking/index.ts
+++ src/common/apis/ranking/index.ts
@@ -1,8 +1,8 @@
 import { rankingInstance } from '../instances';
-import { SearchRanking } from './type';
+import { SearchRankingResponse } from './type';
 
 const rankingApi = {
-  getSearchRanking: () => rankingInstance.get<SearchRanking>(`/search`).then((response) => response.data),
+  getSearchRanking: () => rankingInstance.get<SearchRankingResponse>(`/search`).then((response) => response.data),
 };
 
 export default rankingApi;
