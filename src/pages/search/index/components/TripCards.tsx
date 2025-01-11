186b5f89a160e976922e413987f863f9287cd179
jk <jkwak1635@gmail.com>
2025-01-12 06:58:18 +0900

    refactor: 검색 페이지 타입 이동으로 인한 import 수정

--- src/pages/search/index/components/TripCards.tsx
+++ src/pages/search/index/components/TripCards.tsx
@@ -1,9 +1,7 @@
+import { TripCard } from '@/common/apis/calendar/types';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { TripCard } from '../types';
-
-const DEFAULT_IMAGE =
-  'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+import { DEFAULT_IMAGE } from '../constants';
 
 interface TripCardsProps {
   items: TripCard[];
