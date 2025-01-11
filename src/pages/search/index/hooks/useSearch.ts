9388af82783233fe2433688f46e3e1f902a0ff31
jk <jkwak1635@gmail.com>
2025-01-12 06:45:57 +0900

    feat: calendarApi 적용 및 getSearchItems 단순화

--- src/pages/search/index/hooks/useSearch.ts
+++ src/pages/search/index/hooks/useSearch.ts
@@ -1,21 +1,10 @@
+import { calendarApi } from '@/common/apis/calendar';
+import { SearchParams } from '@/common/apis/calendar/types';
 import { useInfiniteQuery } from '@tanstack/react-query';
-import api from '@/common/apis/api';
-import { SearchParams, SearchResponse } from '../types';
 
 const useSearch = ({ startDate, endDate, page = 0, size = 10 }: SearchParams) => {
-  const getSearchItems = async ({ pageParam = page }: { pageParam: number }) => {
-    try {
-      const response = await api.post<SearchResponse>('/calendar', {
-        startDate,
-        endDate,
-        page: pageParam,
-        size,
-      });
-      return response.data;
-    } catch (error: any) {
-      const errorMessage = error.response?.data.message;
-      throw new Error(errorMessage);
-    }
+  const getSearchItems = async ({ pageParam = 0 }: { pageParam: number }) => {
+    return await calendarApi.postSearchResult({ startDate, endDate, page: pageParam, size });
   };
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isError, error, isLoading } = useInfiniteQuery({
