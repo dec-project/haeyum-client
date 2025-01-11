c248959fdaa9ef61cf510c8937681fe0d8985fa6
jk <jkwak1635@gmail.com>
2025-01-12 06:59:32 +0900

    refactor: data.ts -> constants 로 이동

---
+++ src/pages/search/index/constants/index.ts
@@ -0,0 +1,4 @@
+const DEFAULT_IMAGE =
+  'https://images.unsplash.com/photo-1725714834773-b5f278346915?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
+export { DEFAULT_IMAGE };
