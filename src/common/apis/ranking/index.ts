0800ce4c5cdd6791795dc181f8b24b7c5a7f5452
jk <jkwak1635@gmail.com>
2025-01-12 15:29:29 +0900

    fix: getSearchRanking 호출 수정

--- src/common/apis/ranking/index.ts
+++ src/common/apis/ranking/index.ts
@@ -2,7 +2,7 @@ import { rankingInstance } from '../instances';
 import { SearchRankingResponse } from './type';
 
 const rankingApi = {
-  getSearchRanking: () => rankingInstance.get<SearchRankingResponse>(`/search`).then((response) => response.data),
+  getSearchRanking: rankingInstance.get<SearchRankingResponse>(`/search`).then((response) => response.data),
 };
 
 export default rankingApi;
