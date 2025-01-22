f3cd4834287f180b3b4324c158f3d35c9cf74ca4
jk <jkwak1635@gmail.com>
2025-01-23 04:02:54 +0900

    feat: error handling 일부 개선 및 403 토큰 갱신 처리 추가

--- src/common/apis/interceptors.ts
+++ src/common/apis/interceptors.ts
@@ -1,11 +1,9 @@
 import type { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 import { useAuthStore } from '../stores/useAuthStore';
+import { privateApiInstance } from './instances';
 
 export const requestInterceptor = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
-  const {
-    accessToken,
-    //  refreshToken
-  } = useAuthStore.getState();
+  const { accessToken } = useAuthStore.getState();
 
   if (!config?.headers) {
     return config;
@@ -15,9 +13,6 @@ export const requestInterceptor = (config: InternalAxiosRequestConfig): Internal
     config.headers.Authorization = `Bearer ${accessToken}`;
   }
 
-  // if (refreshToken) {
-  //   config.headers[''] = refreshToken;
-  // }
   return config;
 };
 
@@ -43,15 +38,46 @@ interface ErrorResponse {
 }
 
 export const errorInterceptor = async (error: AxiosError<ErrorResponse>): Promise<void> => {
+  const { config } = error;
+
+  if (!config) {
+    throw new Error('요청이 잘못되었습니다.');
+  }
+
+  const { refreshToken, setTokens, clearTokens } = useAuthStore.getState();
+
+  const handleLogout = (message: string) => {
+    clearTokens();
+    window.location.href = '/login';
+    return Promise.reject(new Error(message));
+  };
+
+  if (!refreshToken) {
+    return handleLogout('Refresh Token이 없습니다. 로그인 페이지로 이동합니다.');
+  }
+
+  if (error.response?.status === 403) {
+    const { code } = error.response.data;
+
+    if (code === 'ET') {
+      try {
+        const response = await privateApiInstance.get('/refresh');
+        const newAccessToken = response.data.accessToken;
+
+        setTokens(newAccessToken, refreshToken);
+        config.headers.Authorization = `Bearer ${newAccessToken}`;
+        return privateApiInstance(config);
+      } catch {
+        return handleLogout('세션이 만료되었습니다. 다시 로그인해주세요.');
+      }
+    }
+
+    return handleLogout('권한이 없습니다. 로그인 페이지로 이동합니다.');
+  }
+
   if (error.response) {
     const { status, data } = error.response;
-
-    const apiError =
-      status >= 400 && status < 500
-        ? new ApiError(status, data?.message || '클라이언트 오류가 발생했습니다.', data)
-        : new ApiError(status, data?.message || '서버 처리 중 오류가 발생했습니다.', data);
-
-    console.error(apiError);
+    const apiError = new ApiError(status, data?.message || '서버 처리 중 오류가 발생했습니다.', data);
     return Promise.reject(apiError);
   } else if (error.request) {
     const apiError = new ApiError(0, '요청은 완료했으나, 서버로부터 응답을 받지 못했습니다.', error.request);
