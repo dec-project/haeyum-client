11d4f909abc8134e27b993a8f5233c5702467729
ohamin26 <ohamin26@gmail.com>
2025-01-24 22:07:04 +0900

    feat : 마이페이지 프로필 수정 api 추가

--- src/common/apis/profile/index.ts
+++ src/common/apis/profile/index.ts
@@ -1,9 +1,16 @@
-import { privateApiInstance } from '../instances';
+import { formDataApiInstance, privateApiInstance } from '../instances';
 import { ProfileFavoriteListResponse, ProfileList } from './types';
 
 export const profileApi = {
   getProfile: async () => {
     const response = await privateApiInstance.get<ProfileList>('/member/profile');
+    return response.data;
+  },
+  putProfile: async (profile: ProfileList) => {
+    const profileFormData = new FormData();
+    profileFormData.append('profileImg', profile.profileImg);
+    profileFormData.append('nickname', profile.nickname);
+    const response = await formDataApiInstance.put<ProfileList>('/member/profile', profileFormData);
     return response.data;
   },
   getProfileFavorite: async () => {
