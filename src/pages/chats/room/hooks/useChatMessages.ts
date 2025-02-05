1a158ffed88ac262afc61924e031f9f6cf1a683d
jk <jkwak1635@gmail.com>
2025-02-05 21:55:18 +0900

    refactor: useQuery 값 반환 방식 수정

--- src/pages/chats/room/hooks/useChatMessages.ts
+++ src/pages/chats/room/hooks/useChatMessages.ts
@@ -1,13 +1,9 @@
 import { chatApi } from '@/common/apis/chatting';
 import { useQuery } from '@tanstack/react-query';
 
-const useChatMessages = (chatroomId: string) => {
-  const query = useQuery({
+export const useChatMessages = (chatroomId: string) => {
+  return useQuery({
     queryKey: ['chatMessages', chatroomId],
     queryFn: () => chatApi.getChatMessages(Number(chatroomId)),
   });
-
-  return { ...query };
 };
-
-export default useChatMessages;
