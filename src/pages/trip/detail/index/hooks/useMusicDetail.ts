b74a3590734380af8bd05f66eea227302b3897ac
jk <jkwak1635@gmail.com>
2025-02-05 23:08:58 +0900

    feat: 로딩 적용

--- src/pages/trip/detail/index/hooks/useMusicDetail.ts
+++ src/pages/trip/detail/index/hooks/useMusicDetail.ts
@@ -1,14 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { tripApi } from '@/common/apis/trip';
 
-const useMusicDetail = (calendarId: string, musicId: string) => {
-  const query = useQuery({
+export const useMusicDetail = (calendarId: string, musicId: string) => {
+  return useSuspenseQuery({
     queryKey: ['music', calendarId, musicId],
     queryFn: () => tripApi.getMusicDetail(calendarId, musicId),
-    enabled: !!calendarId && !!musicId,
   });
-
-  return { ...query };
 };
-
-export default useMusicDetail;
