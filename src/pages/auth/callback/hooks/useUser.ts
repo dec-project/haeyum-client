e33495d84e73760386803f2069e866836cc84ae9
jk <jkwak1635@gmail.com>
2025-02-13 18:38:59 +0900

    feat: 로그인 로직 개선

--- src/pages/auth/callback/hooks/useUser.ts
+++ src/pages/auth/callback/hooks/useUser.ts
@@ -1,8 +1,26 @@
-import { userApi } from '@/common/apis/user';
 import { useMutation } from '@tanstack/react-query';
+import { useAuthStore } from '@/common/stores/useAuthStore';
+import { useNavigate } from 'react-router-dom';
+import { userApi } from '@/common/apis/user';
 
 export const useUser = () => {
+  const { setTokens } = useAuthStore();
+  const navigate = useNavigate();
+
   return useMutation({
     mutationFn: (code: string) => userApi.postAuthCode(code),
+    onSuccess: (data) => {
+      const { accessToken, refreshToken, socialSub: userId } = data;
+
+      if (!accessToken || !refreshToken || !userId) {
+        throw new Error('토큰 값을 확인해주세요.');
+      }
+
+      setTokens(accessToken, refreshToken, userId);
+      navigate('/');
+    },
+    onError: () => {
+      navigate('/login');
+    },
   });
 };
